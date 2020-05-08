f75a96ef3da2f201d1f62b58063f1f840f17383f
nyLiao <39255546+nyLiao@users.noreply.github.com>
2020-05-08 14:25:39 +0800

    Haystack+whoosh work

--- mysite/search/models.py
+++ mysite/search/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class ArticleType(models.Model):
+    # id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=32)     # web title
+    date = models.DateField()                   # web date
+    url = models.CharField(max_length=64)       # web url
+    view = models.IntegerField()                # web views times
+    category = models.CharField(max_length=32)  # web class, tags, etc
+    content = models.TextField()                # web content
