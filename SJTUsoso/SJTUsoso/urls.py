4f26db0c898f975daf0d8ae55ba0fcd7a0ee2be4
nyLiao <39255546+nyLiao@users.noreply.github.com>
2020-05-29 14:36:49 +0800

    Revert "Revert "Merge branch 'work-nyLiao'""
    This reverts commit 9c8d17484753e6a47240ebe946044154a655cb10.

--- SJTUsoso/SJTUsoso/urls.py
+++ SJTUsoso/SJTUsoso/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 from blog.views import *
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.conf.urls import url
+from django.views import static
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from haystack.views import SearchView
+
 from soso.views import *
 from django.conf.urls import url
 from django.views import static
@@ -39,7 +44,8 @@ urlpatterns = [
     path('register/', register),
     path('reset/', reset),
     path('single/<int:Wechat_id>/', tosingle ,name="Wechat"),
-    path('search/', SearchView(), name='haystack_search'),
+    path('search/', mySearchView.as_view(), name='haystack_search'),
+    path('search/autocomplete/', autocomplete),
     path('contactsuccess/', contactsuccess),
     path('contactfail/', contactfail),
     path('logout/', logout),
@@ -60,3 +66,5 @@ urlpatterns = [
     url(r'^css/safari\.css$', RedirectView.as_view(url=r'/static/css/safari.css')),
     re_path(r'mdeditor/', include('mdeditor.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
